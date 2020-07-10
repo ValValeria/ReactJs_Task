e85c73c6a89267f4902a4b2f12d38258ac6ff76f
Vasiliy Pupkin <led2017l@gmail.com>
2020-07-10 17:07:17 +0300

    Updates

---
+++ precache-manifest.2796293a99e13169ab21ede8e01363f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4992119ccafd7cba0a56d7f19737c9e",
+    "url": "/ReactJs_Task/index.html"
+  },
+  {
+    "revision": "f68b274f4bb69aa29737",
+    "url": "/ReactJs_Task/static/css/main.3059748c.chunk.css"
+  },
+  {
+    "revision": "d42292445f4994584fee",
+    "url": "/ReactJs_Task/static/js/2.c1ff758e.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/ReactJs_Task/static/js/2.c1ff758e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f68b274f4bb69aa29737",
+    "url": "/ReactJs_Task/static/js/main.89ab0caa.chunk.js"
+  },
+  {
+    "revision": "58ba51dfb67fc3c10ff7",
+    "url": "/ReactJs_Task/static/js/runtime-main.187d3c4b.js"
+  }
+]);
